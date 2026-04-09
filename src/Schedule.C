6a1a7ba905f783288bb779be4febbb067ad135f0
Paul P.H. Wilson <paul.wilson@wisc.edu>
2026-04-09 03:00:20 -0500

    light change to find top_sched

--- src/Schedule.C
+++ src/Schedule.C
@@ -184,29 +184,23 @@ void Schedule::write(int level, char *histName, double delay, char dUnits)
   /* on first call, search for top level schedule */
   if (level==0)
     {
-      bool found_top_sched = FALSE;
+      Schedule *top_sched = NULL;
       verbose(0,"\n\n***Please review this schedule hierarchy.!!!!!!!!!!\n");
       while (ptr->next != NULL)
 	    {
 	    ptr = ptr->next;
       if (!ptr->usedAsSub)
         {
-        found_top_sched = TRUE;
-        break;
+          if (top_sched != NULL)
+            error(400, "Multiple top schedules have been found. Only one schedule can be the top schedule.");
+        top_sched = ptr;
         }  
 	    }
-      Schedule *next_sched = ptr->next;
-      while (next_sched != NULL && found_top_sched == TRUE)
-      {
-        if (!next_sched->usedAsSub)
-          error(400, "Multiple top schedules have been found. Only one schedule can be the top schedule.");
-      next_sched = next_sched->next;
-      }
       
-      if (found_top_sched == FALSE)
+      if (top_sched == NULL)
 	error(400,"Unable to find top level schedule.\nA top level schedule must not used as a sub-schedule.");
 
-      cout << "top_schedule '" << ptr->schedName << "':" << endl;
+      cout << "top_schedule '" << top_sched->schedName << "':" << endl;
     }
   else
     {
